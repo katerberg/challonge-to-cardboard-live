fc6705f213a8f558e264deab64d7ed3f13224b22
Mark Katerberg <katerberg@fastmail.fm>
2020-01-29 22:42:11 -0600

    Makes login creds work but doesn't handle expirations gracefully yet

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
   getTournamentResults,
 } = require('./src/challonge');
 const {
+  login,
   uploadToCardboardLive,
 } = require('./src/cardboardLive');
 const cardboardLiveCreds = require('./creds/cardboardLive');
@@ -20,26 +21,39 @@ if (!cardboardLiveCreds.tournament) {
   process.exit(1);
 }
 
-console.debug(`Getting ${challongeCreds.tournament} tournament results`);
-getTournamentResults(challongeCreds.tournament).then(results => {
-  const html = transformResultsToHtml(results);
-  const players = getPlayers(results);
-  const round = getRound(results);
-  fs.writeFile(`${process.cwd()}/rd${getRound(results)}-standings.html`, html, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.debug(`Uploading results to ${cardboardLiveCreds.tournament}`);
-    uploadToCardboardLive(html, cardboardLiveCreds.tournament, round)
-      .then(() => console.debug('successful upload'))
-      .catch(e => console.error(e));
+console.debug('Checking for stored token'); 
+fs.readFile('./creds/cardboardToken.json', 'utf8', async (err, data) => {
+  let token;
+  if (err) {
+    console.debug('No token found. Logging in again'); 
+    const result = await login(cardboardLiveCreds.username, cardboardLiveCreds.password);
+    token = result.access_token;
+  } else {
+    console.debug('Token found. Proceeding.'); 
+    token = JSON.parse(data).access_token;
+  }
+
+  console.debug(`Getting ${challongeCreds.tournament} tournament results`);
+  getTournamentResults(challongeCreds.tournament).then(results => {
+    const html = transformResultsToHtml(results);
+    const players = getPlayers(results);
+    const round = getRound(results);
+    fs.writeFile(`${process.cwd()}/rd${getRound(results)}-standings.html`, html, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.debug(`Uploading results to ${cardboardLiveCreds.tournament}`);
+      uploadToCardboardLive(token, html, cardboardLiveCreds.tournament, round)
+        .then(() => console.debug('successful upload'))
+        .catch(e => console.error(e));
+    });
+    fs.writeFile(`${process.cwd()}/players.csv`, players, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }).catch(() => {
+    console.error('Tournament not found');
+    process.exit(1);
   });
-  fs.writeFile(`${process.cwd()}/players.csv`, players, (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-}).catch(() => {
-  console.error('Tournament not found');
-  process.exit(1);
 });
--- src/cardboardLive.js
+++ src/cardboardLive.js
@@ -1,11 +1,12 @@
+const fs = require('fs');
 const creds = require('../creds/cardboardLive.json');
 const axios = require('axios');
 
-function uploadToCardboardLive(html, tournamentId, roundNumber) {
-  const boundry = '-----------------------------206411090965099725487092284';
+function uploadToCardboardLive(token, html, tournamentId, roundNumber) {
+  const boundary = '-----------------------------206411090965099725487092284';
   const header = `Content-Disposition: form-data; name="file"; filename="rd${roundNumber}-standings.html"`;
   const contentType = 'Content-Type: text/html';
-  const body = `${boundry}\n${header}\n${contentType}\n\n${html}\n${boundry}--\n`;
+  const body = `${boundary}\n${header}\n${contentType}\n\n${html}\n${boundary}--\n`;
   const options = {
     headers: {
       Accept: 'application/json, text/plain, */*',
@@ -17,6 +18,23 @@ function uploadToCardboardLive(html, tournamentId, roundNumber) {
   return axios.post(`https://app.cardboard.live/api/api/v1/tournaments/${tournamentId}/import_standings`, body, options);
 }
 
+function login(username, password) {
+  const options = {
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      Authorization: 'undefined',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+  const body = `grant_type=&username=${encodeURIComponent(username)}&client_id=1&client_secret=secret&password=${password}&grant_type=password`;
+
+  return axios.post('https://app.cardboard.live/api/oauth/v2/token', body, options).then(r => r.data).then(token => {
+    fs.writeFile(`${process.cwd()}/creds/cardboardToken.json`, JSON.stringify({...token, date: new Date().valueOf()}), () => {});
+    return token;
+  });;
+}
+
 module.exports = {
+  login,
   uploadToCardboardLive,
 };
